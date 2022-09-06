7c74c393c8ca27790e53680701926fa66ca0c59f
Daniel Too <dan@emojieggplant.com>
2022-09-06 17:15:45 +1200

    updated for ios versions

--- RichEditorView/Classes/RichEditorWebView.swift
+++ RichEditorView/Classes/RichEditorWebView.swift
@@ -31,28 +31,55 @@ public class RichEditorWebView: WKWebView {
         }
     }
 
-    private func setKeyboardRequiresUserInteraction(_ value: Bool) {
-        guard let WKContentViewClass: AnyClass = NSClassFromString("WKContentView") else {
-            return print("Cannot find WKContentView class")
+    private func setKeyboardRequiresUserInteraction( _ value: Bool) {
+        guard let WKContentView: AnyClass = NSClassFromString("WKContentView") else {
+            print("keyboardDisplayRequiresUserAction extension: Cannot find the WKContentView class")
+            return
         }
-
-        let oldSelector: Selector = sel_getUid("_startAssistingNode:userIsInteracting:blurPreviousNode:userObject:")
-        let newSelector: Selector = sel_getUid("_startAssistingNode:userIsInteracting:blurPreviousNode:changingActivityState:userObject:")
-
-        if let method = class_getInstanceMethod(WKContentViewClass, oldSelector) {
+        // For iOS 10, *
+        let sel_10: Selector = sel_getUid("_startAssistingNode:userIsInteracting:blurPreviousNode:userObject:")
+        // For iOS 11.3, *
+        let sel_11_3: Selector = sel_getUid("_startAssistingNode:userIsInteracting:blurPreviousNode:changingActivityState:userObject:")
+        // For iOS 12.2, *
+        let sel_12_2: Selector = sel_getUid("_elementDidFocus:userIsInteracting:blurPreviousNode:changingActivityState:userObject:")
+        // For iOS 13.0, *
+        let sel_13_0: Selector = sel_getUid("_elementDidFocus:userIsInteracting:blurPreviousNode:activityStateChanges:userObject:")
+        
+        if let method = class_getInstanceMethod(WKContentView, sel_10) {
             let originalImp: IMP = method_getImplementation(method)
             let original: OldClosureType = unsafeBitCast(originalImp, to: OldClosureType.self)
-            let block: @convention(block) (Any, UnsafeRawPointer, Bool, Bool, Any?) -> Void = { (me, arg0, arg1, arg2, arg3) in
-                original(me, oldSelector, arg0, !value, arg2, arg3)
+            let block : @convention(block) (Any, UnsafeRawPointer, Bool, Bool, Any?) -> Void = { (me, arg0, arg1, arg2, arg3) in
+                original(me, sel_10, arg0, !value, arg2, arg3)
             }
             let imp: IMP = imp_implementationWithBlock(block)
             method_setImplementation(method, imp)
         }
-        if let method = class_getInstanceMethod(WKContentViewClass, newSelector) {
+        
+        if let method = class_getInstanceMethod(WKContentView, sel_11_3) {
             let originalImp: IMP = method_getImplementation(method)
             let original: NewClosureType = unsafeBitCast(originalImp, to: NewClosureType.self)
-            let block: @convention(block) (Any, UnsafeRawPointer, Bool, Bool, Bool, Any?) -> Void = { (me, arg0, arg1, arg2, arg3, arg4) in
-                original(me, newSelector, arg0, !value, arg2, arg3, arg4)
+            let block : @convention(block) (Any, UnsafeRawPointer, Bool, Bool, Bool, Any?) -> Void = { (me, arg0, arg1, arg2, arg3, arg4) in
+                original(me, sel_11_3, arg0, !value, arg2, arg3, arg4)
+            }
+            let imp: IMP = imp_implementationWithBlock(block)
+            method_setImplementation(method, imp)
+        }
+        
+        if let method = class_getInstanceMethod(WKContentView, sel_12_2) {
+            let originalImp: IMP = method_getImplementation(method)
+            let original: NewClosureType = unsafeBitCast(originalImp, to: NewClosureType.self)
+            let block : @convention(block) (Any, UnsafeRawPointer, Bool, Bool, Bool, Any?) -> Void = { (me, arg0, arg1, arg2, arg3, arg4) in
+                original(me, sel_12_2, arg0, !value, arg2, arg3, arg4)
+            }
+            let imp: IMP = imp_implementationWithBlock(block)
+            method_setImplementation(method, imp)
+        }
+        
+        if let method = class_getInstanceMethod(WKContentView, sel_13_0) {
+            let originalImp: IMP = method_getImplementation(method)
+            let original: NewClosureType = unsafeBitCast(originalImp, to: NewClosureType.self)
+            let block : @convention(block) (Any, UnsafeRawPointer, Bool, Bool, Bool, Any?) -> Void = { (me, arg0, arg1, arg2, arg3, arg4) in
+                original(me, sel_13_0, arg0, !value, arg2, arg3, arg4)
             }
             let imp: IMP = imp_implementationWithBlock(block)
             method_setImplementation(method, imp)
